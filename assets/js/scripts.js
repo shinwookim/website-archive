13b2c497fdcb1e7b552f948ef4f0ba0882de8f1b
Shinwoo Kim <shinwoo.k0370@gmail.com>
2022-06-11 21:11:00 -0400

    Add Recitation page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,25 +4,74 @@
 const select = (el, all = false) => {
     el = el.trim()
     if (all) {
-        return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)]
     } else {
-        return document.querySelector(el)
+      return document.querySelector(el)
     }
-}
-
-/*
- * Typing Effect
- */
- const typed = select('.typed')
- if (typed) {
-     let typed_strings = typed.getAttribute('data-typed-items')
-     typed_strings = typed_strings.split(',')
-     new Typed('.typed', {
-         strings: typed_strings,
-         loop: true,
-         typeSpeed: 100,
-         backSpeed: 50,
-         backDelay: 2000
-     });
- }
- 
+  }
+  
+  /*
+   * Typing Effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+  
+  function togglePrint() {
+    if (document.getElementById("mainNav").classList.contains("printmode")) {
+      togglePrintOff();
+    } else {
+      togglePrintOn();
+      window.print();
+    }
+  }
+  
+  
+  function togglePrintOn() {
+    document.getElementById("main-withsidebar").classList.add("mx-auto");
+    document.getElementById("main-withsidebar").classList.remove("border");
+    document.getElementById("main-withsidebar").style.backgroundColor = "white";
+    document.getElementById("main-withsidebar").classList.remove("left-border-side");
+    document.getElementById("page-container").style.marginTop = "0px";
+    document.getElementById("page-container").style.marginTop = "0px";
+    document.getElementById("mainNav").classList.add("printmode");
+    document.body.style.marginTop = "0px";
+    document.getElementById("side-menu").classList.add("printmode");
+    document.getElementById("side-menu").classList.add("printmode");
+    document.getElementById("footer").classList.add("printmode");
+    document.getElementById("footer").classList.remove("bg-dark");
+  }
+  
+  function togglePrintOff() {
+    document.getElementById("main-withsidebar").classList.remove("mx-auto");
+    document.getElementById("main-withsidebar").classList.add("border");
+    document.getElementById("main-withsidebar").style.backgroundColor = "#fffff8";
+    document.getElementById("main-withsidebar").classList.add("left-border-side");
+    document.getElementById("page-container").style.marginTop = "4em";
+    document.getElementById("mainNav").classList.remove("printmode");
+    document.body.style.marginTop = "74px";
+    document.getElementById("side-menu").classList.remove("printmode");
+    document.getElementById("side-menu").classList.remove("printmode");
+    document.getElementById("footer").classList.remove("printmode");
+    document.getElementById("footer").classList.add("bg-dark");
+  }
+  
+  function newStyle() {
+    const classname = "CSXXXX";
+    const newNavName = classname + "";
+    document.getElementById("nav-brand").innerHTML = newNavName;
+    document.getElementById("nav-brand").classList.remove('titillium-bold');
+    document.getElementById("nav-brand").classList.add('h1');
+    document.getElementById("nav-brand").classList.add('small-caps');
+    document.getElementById("main-withsidebar").style.backgroundColor = "#fffff8";
+    document.getElementById("main-withsidebar").classList.add("mb-2");
+  }
---
+++ teaching/CSXXXX/index.md
@@ -0,0 +1,60 @@
+---
+layout: page
+title: CSXXXX Lauren Ipsum Intro
+published: false
+classStyle: true
+---
+
+Welcome to **CS-XXXX**, Lauren Ipsum Intro, for the Fall `XXXX` term.
+
+---
+
+## Annonucements
+
+| Date           | Annonucement       |
+| -------------- | ------------------ |
+| **09/01/1995** | First day of class |
+
+---
+
+## Course Information
+
+**Lectures:** Tue/Thu, 4:00–5:15pm  
+**Classroom:** Lawrence 107  
+**Section:** `XXX` (**CRN:** `XXXXX`)  
+**Instructor:** Dr Heinz Doofenshmirtz  
+_See most accurate information on [Canvas](https://canvas.pitt.edu)_  
+**Course Description**
+
+> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus leo, maximus non consectetur a, posuere in mauris. Cras a mattis arcu. Etiam sed laoreet mi. Cras nec ipsum nibh. Sed eu scelerisque urna. Pellentesque lacus velit, condimentum molestie risus scelerisque, eleifend pharetra eros. In eget turpis feugiat, aliquam nisl non, congue arcu. Sed tempor quam vitae lorem ullamcorper, ac luctus risus sodales. In luctus, massa eget sagittis pulvinar, diam sapien aliquet arcu, vitae lobortis libero tortor quis purus. Curabitur auctor blandit tortor. Morbi sit amet sodales velit, eu pellentesque nunc. Fusce ac dui et diam imperdiet dapibus id eget orci.
+
+### Recitation Instructor Information
+
+- TA: Shinwoo Kim
+- Email: [`shinwookim@pitt.edu`](mailto:shiwookim@pitt.edu)
+- TA Office Hours
+  - Time: TBD; By Appointment
+  - Location: TBD
+
+> **When Emailing:**  
+> Append `CS-XXXX` to ALL E-mail communication subject lines. I will lose track of emails that do not include that in the subject.
+
+---
+
+## Recitation Schedule
+
+| Date                        | Topic                         | Handouts                                                | Remarks                       |
+| --------------------------- | ----------------------------- | ------------------------------------------------------- | ----------------------------- |
+| Monday, September 1st, 2022 | 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus leo, maximus non consectetur a, posuere in mauris. | Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus leo, maximus non consectetur a, posuere in mauris. | Material Courtsey of Dr. XXX |
+
+## Helpful Links
+
+[University of Pittsburgh, Department of Computer Science](https://cs.pitt.edu)  
+<br />
+
+> **DISCLAIMER**
+> All materials provided for this course are protected by copyright. United States copyright law, 17 USC § 101, et seq., in addition to University policy and procedures, prohibit unauthorized duplication or retransmission of course materials. This applies to all class notes, slides, assignments, solutions, project descriptions, quiz and exam materials, etc. You are allowed (and indeed, expected) to use all provided material for personal academic use. However, you are strictly prohibited from sharing any materials with others in general and from posting the material on the Web or other file-sharing venues in particular.
+
+<script>
+  window.onload = function() {newStyle()};
+</script>
